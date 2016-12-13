b0a50993ee53398ba2b108ee925c9db9f19b7b91
Zdenek <zd.brabec@gmail.com>
2016-12-13 13:13:10 +0100

    reading input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,59 @@
 #include <iostream>
 
+struct Edge;
+struct State;
+
+struct State {
+    bool is_final = false;
+    Edge *heads;
+};
+
+struct Edge {
+    Edge *next;
+    State *end;
+};
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+
+    int states_count, alphabet_size;
+    State *states;
+
+    std::cin >> states_count;
+    std::cin >> alphabet_size;
+
+    states = new State[states_count];
+
+    for(int i=0;i<states_count;i++) {
+
+        int state; char is_final;
+        scanf("%d %c", &state, &is_final);
+        states[i].is_final = is_final == 'F';
+        states[i].heads = new Edge[alphabet_size];
+
+        for(int j=0;j<alphabet_size;j++) {
+            Edge cur = states[i].heads[j];
+
+            char character;
+            scanf(" %c",&character);
+
+            int symbol; char ch;
+            while(scanf("%c",&ch) > 0 && ch != 0x0a && scanf("%d",&symbol) > 0) {
+                cur.end = &states[symbol];
+                cur.next = new Edge;
+                cur = *cur.next;
+            }
+
+        }
+
+    }
+
+    // todo read prefix here
+
+
+
+    delete [] states;
+
     return 0;
 }
